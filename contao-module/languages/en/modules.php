2b025d0e53495fdbbbf423bf2eb579ec9a4917e2
Nicky Hoff <nick@hofff.com>
2017-09-12 14:44:13 +0200

    add title for module

--- contao-module/languages/en/modules.php
+++ contao-module/languages/en/modules.php
@@ -1,3 +1,4 @@
 <?php
 
 $GLOBALS['TL_LANG']['CTE']['hofff_tawk-to'] = array('Tawk.to Plugin', 'Tawk.to Plugin');
+$GLOBALS['TL_LANG']['FMD']['hofff_tawk-to'] = array('Tawk.to Plugin', 'Tawk.to Plugin');
